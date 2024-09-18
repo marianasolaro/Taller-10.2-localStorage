7b5ff3cb15caa7fd0aea7b19a00182f37c080b54
marianasolaro <solaromariana@gmail.com>
2024-09-18 20:47:14 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 document.getElementById('buttonText').addEventListener('click', function() {
     let inputText = document.getElementById('inputText').value;
     localStorage.setItem('datoGuardado', inputText);
+    document.getElementById('inputText').value = '';
+    
+    alert('Dato guardado con éxito')
 })
